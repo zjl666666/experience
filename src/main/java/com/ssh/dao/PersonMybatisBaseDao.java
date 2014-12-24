e1f69ed06a8819e2b55e77c19ad4623becaee4ef
zjl <181988764@qq.com>
2014-12-24 11:44:50 +0800

    加入mybatis及mybatis与spring集成的代码

--- src/main/java/com/ssh/dao/PersonMybatisBaseDao.java
+++ src/main/java/com/ssh/dao/PersonMybatisBaseDao.java
@@ -1,11 +1,14 @@
 package com.ssh.dao;
 
+<<<<<<< Upstream, based on feature-ssh
 /**
  *  
  * @Description: mybatis自动探测的时候使用
  * @author zjl
  * @date 2014年12月26日 上午10:46:32
  */
+=======
+>>>>>>> dd1f737 加入mybatis及mybatis与spring集成的代码
 public interface PersonMybatisBaseDao {
 
 }
