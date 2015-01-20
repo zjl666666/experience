bd8ddafb192d2bf53ed027e8a4de1f6705791c00
zjl <181988764@qq.com>
2015-01-20 17:23:44 +0800

    error

--- src/test/java/com/ssh/dao/impl/TestMongodbDaoDummyImpl.java
+++ src/test/java/com/ssh/dao/impl/TestMongodbDaoDummyImpl.java
@@ -9,8 +9,7 @@ import org.slf4j.LoggerFactory;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
-import com.mongodb.dao.MongodbDao;
-import com.mongodb.dao.impl.MongodbDaoUrlImpl;
+import com.ssh.dao.MongodbDao;
 
 public class TestMongodbDaoDummyImpl {
 	
