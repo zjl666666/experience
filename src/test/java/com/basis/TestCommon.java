8c2f89666fd8e1858a33face4139d0aebd9cff7f
zjl <181988764@qq.com>
2015-03-05 15:28:14 +0800

    加入JMX demo

--- src/test/java/com/basis/TestCommon.java
+++ src/test/java/com/basis/TestCommon.java
@@ -1,18 +1,19 @@
 package com.basis;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.search.service.impl.TestIndexServiceImpl;
 
 public class TestCommon {
 	
 	private static final Logger logger = LoggerFactory.getLogger(TestCommon.class);
 
 	public static void main(String[] args) {
-		String str="test";
-		String str1="test";
-	    logger.debug("测试是否相同的字符串只生成一个"+(str==str1));
+		List<String> listStr=new LinkedList<String>();
+		listStr.add("test");
+		listStr.add("test1");
 	}
 
 }
